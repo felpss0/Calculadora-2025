e726cb800a47c86eb061829ddd1ff5013c7844c4
Filipe Straubel <fstraubel10@gmail.com>
2025-03-06 17:31:19 -0300

    Adicionada função de subtração

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,17 +4,40 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------------------------");
             Console.WriteLine("Calculadora Tabajara 2025");
+            Console.WriteLine("-----------------------------");
 
-            Console.Write("Digite o Primeiro numero");
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("S - Sair");
+
+            Console.Write("Escolha uma opção: ");
+
+            string opcao = Console.ReadLine().ToUpper();
+
+            if (opcao == "S") {
+                return;
+            } 
+
+                Console.Write("Digite o Primeiro numero: ");
             string num1String = Console.ReadLine();
             int num1 = Convert.ToInt32(num1String);
 
-            Console.Write("Digite o Segundo numero");
+            Console.Write("Digite o Segundo numero: ");
             string num2String = Console.ReadLine();
             int num2 = Convert.ToInt32(num2String);
 
-            int resultado = num1 + num2;
+            int resultado = 0;
+
+            if (opcao == "1") {
+                 resultado = num1 + num2;
+            } else 
+            {
+                resultado = num1 - num2;
+            }
+
+
 
             Console.WriteLine(resultado);
 
