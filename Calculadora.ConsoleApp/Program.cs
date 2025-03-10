a2382c3fe437e14e62f4abcbf48fc9b0d83cb5ae
Filipe Straubel <fstraubel10@gmail.com>
2025-03-10 14:51:45 -0300

    Adicionado multiplicação e divisão e implementado suporte a casas decimais e funcionalidade de execução em loop

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,44 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine("Calculadora Tabajara 2025");
-            Console.WriteLine("-----------------------------");
+    
+                while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Calculadora Tabajara 2025");
+                Console.WriteLine("-----------------------------");
 
-            Console.WriteLine("1 - Somar");
-            Console.WriteLine("2 - Subtrair");
-            Console.WriteLine("S - Sair");
+                Console.WriteLine("1 - Somar");
+                Console.WriteLine("2 - Subtrair");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão");
+                Console.WriteLine("S - Sair");
 
-            Console.Write("Escolha uma opção: ");
+                Console.Write("Escolha uma opção: ");
 
-            string opcao = Console.ReadLine().ToUpper();
+                string opcao = Console.ReadLine().ToUpper();
 
-            if (opcao == "S") {
-                return;
-            } 
+                if (opcao == "S")
+                    break;
 
                 Console.Write("Digite o Primeiro numero: ");
-            string num1String = Console.ReadLine();
-            int num1 = Convert.ToInt32(num1String);
+                string num1String = Console.ReadLine();
+                decimal num1 = Convert.ToDecimal(num1String);
 
-            Console.Write("Digite o Segundo numero: ");
-            string num2String = Console.ReadLine();
-            int num2 = Convert.ToInt32(num2String);
+                Console.Write("Digite o Segundo numero: ");
+                string num2String = Console.ReadLine();
+                decimal num2 = Convert.ToDecimal(num2String);
 
-            int resultado = 0;
+                decimal resultado = 0;
 
-            if (opcao == "1") {
-                 resultado = num1 + num2;
-            } else 
-            {
-                resultado = num1 - num2;
+                if (opcao == "1")
+                {
+                    resultado = num1 + num2;
+                }
+                else if (opcao == "2")
+                {
+                    resultado = num1 - num2;
+                }
+                else if (opcao == "3")
+                {
+                    resultado = num1 * num2;
+                }
+                else if (opcao == "4") 
+                {
+                    resultado = num1 / num2;
+                }
+
+
+                    Console.WriteLine("-----------------------------");
+                Console.Write("O resultado é: " + resultado.ToString("F2"));
+                Console.WriteLine("-----------------------------");
+
+                Console.Write("Deseja continuar? (S/N)");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S") 
+                    break;
+
+                Console.ReadLine();
             }
 
-
-
-            Console.WriteLine(resultado);
-
-            Console.ReadLine();
+           
 
 
 
